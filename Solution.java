474e9c24e92c2c7293a15c0360a1143eb2b8c8e7
Jose Carlos <jose.gonzalezh@protonmail.com>
2025-03-18 16:41:17 +0000

    Añadido del javadoc.

--- Solution.java
+++ Solution.java
@@ -1,6 +1,10 @@
 import java.lang.StringBuilder;
 public class Solution {
-
+    /**
+     *
+     * @param s Mediante la s, le pasamos el string que queremos convertir.
+     * @return Devolvemos el string que hemos pasado ya convertido en camelCase.
+     */
    public static String toCamelCase(String s){
         StringBuilder resultado = new StringBuilder();
         String[] palabras = s.split("[-_]");
